2976a00435cd998545c88453c4a87d3ddc529b1b
Mahad Kalam <mahadkalam1@proton.me>
2025-05-30 22:53:41 +0100

    Update the icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "wxt";
 
 export default defineConfig({
   manifest: {
+    name: "Clippy Two",
     host_permissions: ["<all_urls>"],
     permissions: ["offscreen", "tts"],
   },
