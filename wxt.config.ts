ea5c51e889ffcdbedfb15290bc82796d6875d78b
Mahad Kalam <mahadkalam1@proton.me>
2025-05-30 23:16:27 +0100

    Remove unused permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   manifest: {
     name: "Clippy Two",
     host_permissions: ["<all_urls>"],
-    permissions: ["offscreen", "tts"],
   },
   modules: ["@wxt-dev/module-react"],
 });
